87e23d86748c3547311e9594cfa88408ee27b329
Hichiki13 <safeelbazofficial@gmail.com>
2023-12-19 15:35:09 +0100

    kk

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""define a class square with a size"""
+
+
+class Square:
+    """represent  a square"""
+
+    def __init__(self, size):
+        """initialize a new square
+        Args:
+        size (int): the size of the new square
+        """
+        self.__size = size
